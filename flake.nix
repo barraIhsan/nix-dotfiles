9a8161aa59a9c6fcf219fc988fa9dbcf05d56c67
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2026-05-28 19:29:07 +0700

    feat: initialize nix flakes

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "my system :3";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+
+        config = {
+          allowUnfree = true;
+        };
+      };
+
+    in
+    {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./nixos/configuration.nix
+          ];
+        };
+      };
+    };
+}
