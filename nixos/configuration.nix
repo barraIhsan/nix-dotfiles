c9ca53d609aa77184d4dace1d1d9252a51f6b182
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2026-05-29 10:05:31 +0700

    feat: add fonts (noto, twemoji, jetbrains nerd)
    The configuration.nix is still messy and im planning to make another
    `modules` in root for system packages (its different from HM's `modules`)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -112,6 +112,22 @@
     wakatime-cli
   ];
 
+  # fonts
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+
+      twemoji-color-font
+      nerd-fonts.jetbrains-mono
+    ];
+
+    # https://wiki.nixos.org/wiki/Fonts#Flatpak_applications_can't_find_system_fonts
+    fontDir.enable = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
