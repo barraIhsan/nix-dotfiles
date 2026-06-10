9109447448d5d1b61b4d99b167791cf900db80eb
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2026-06-10 14:30:59 +0700

    fix(nvim): forgot to actually link the config

--- home/modules/nvim.nix
+++ home/modules/nvim.nix
@@ -1,8 +1,11 @@
-{ pkgs, ... }: {
-  programs.neovim = {
-    enable = true;
+{ config, pkgs, ... }: {
+  xdg.configFile."nvim" = {
+    source = config.lib.file.mkOutOfStoreSymlink ../config/nvim;
+    recursive = false;
   };
+
   home.packages = with pkgs; [
+    neovim
     git
     gcc
     gnumake
