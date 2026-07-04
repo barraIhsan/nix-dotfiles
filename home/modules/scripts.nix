37a053bf6ce905700c9caa98669df726d25e7092
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2026-07-05 01:58:33 +0700

    feat(script): make it modular
    So that i can just add scripts on `home/modules` and add "deps" as
    well... deps

--- home/modules/scripts.nix
+++ home/modules/scripts.nix
@@ -1,13 +1,35 @@
 { pkgs, ... }:
+let
+  scriptsDir = ../scripts;
+
+  scriptFiles = pkgs.lib.filterAttrs (name: type: type == "regular") (builtins.readDir scriptsDir);
+
+  scriptNames = builtins.attrNames scriptFiles;
+
+  # pull out "# deps: a b c" from a script's text, return list of strings
+  parseDeps =
+    text:
+    let
+      lines = pkgs.lib.splitString "\n" text;
+      depLine = pkgs.lib.findFirst (l: pkgs.lib.hasPrefix "# deps:" l) null lines;
+    in
+    if depLine == null then
+      [ ]
+    else
+      pkgs.lib.splitString " " (pkgs.lib.removePrefix "# deps: " depLine);
+
+  mkScript =
+    name:
+    let
+      text = builtins.readFile (scriptsDir + "/${name}");
+      depNames = parseDeps text;
+    in
+    pkgs.writeShellApplication {
+      inherit name;
+      runtimeInputs = map (n: pkgs.${n}) depNames;
+      text = text;
+    };
+in
 {
-  home.packages = [
-    (pkgs.writeShellApplication {
-      name = "xdg-fakehome";
-      runtimeInputs = with pkgs; [
-        coreutils
-        findutils
-      ];
-      text = builtins.readFile ../scripts/xdg-fakehome;
-    })
-  ];
+  home.packages = map mkScript scriptNames;
 }
