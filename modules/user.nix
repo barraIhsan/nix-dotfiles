d83c37431da3989ce9cc1226cb49e1aea013a0f3
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2026-07-26 06:54:41 +0700

    feat: run docker without sudo

--- modules/user.nix
+++ modules/user.nix
@@ -1,10 +1,12 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.barra = {
     isNormalUser = true;
     extraGroups = [
       "networkmanager"
       "wheel"
+      "docker"
     ];
     shell = pkgs.zsh;
   };
