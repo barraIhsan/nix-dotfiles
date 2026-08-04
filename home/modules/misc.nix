57248bc4c05efe2b349338fab2f7bd48e60aebbc
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2026-08-04 14:34:05 +0700

    fix: fix weird combobox on gtk on plasma

--- home/modules/misc.nix
+++ home/modules/misc.nix
@@ -23,4 +23,9 @@
   services.syncthing = {
     enable = true;
   };
+
+  # gtk config
+  # Breeze GTK weird combobox menu scroll arrow bug at certain font sizes
+  # https://www.reddit.com/r/kde/comments/1ky6b41/has_anyone_found_a_fix_for_the_gtk_breeze/muuuc2u/
+  xdg.configFile."gtk-3.0/gtk.css".source = ../config/gtk/gtk-3.0/gtk.css;
 }
