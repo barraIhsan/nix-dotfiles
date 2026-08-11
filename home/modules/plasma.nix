2f54c21c163ef444d0bdb16ef66090e3e47081e4
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2026-08-12 03:36:17 +0700

    fix(plasma): update discord icon name on taskbar/panel

--- home/modules/plasma.nix
+++ home/modules/plasma.nix
@@ -102,7 +102,7 @@
                   "org.kde.plasma.vault"
                   "org.kde.plasma.weather"
                   "plasmashell_microphone"
-                  "chrome_status_icon_1@.Discord-wrappe" # discord
+                  "discord_status_icon_1"
                 ];
                 # currently doesn't work
                 # see https://github.com/nix-community/plasma-manager/issues/565#issuecomment-3837849097
