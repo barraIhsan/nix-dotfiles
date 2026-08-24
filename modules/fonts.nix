22ecdad97ef04f67c8789dc854775bcd95184c43
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2026-08-25 01:40:01 +0700

    feat: install corefonts

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,8 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   # fonts
   fonts = {
     packages = with pkgs; [
+      corefonts
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
@@ -16,4 +17,7 @@
     # https://wiki.nixos.org/wiki/Fonts#Flatpak_applications_can't_find_system_fonts
     fontDir.enable = true;
   };
+
+  # https://wiki.nixos.org/wiki/ONLYOFFICE#Install_and_use_missing_corefonts
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "corefonts" ];
 }
